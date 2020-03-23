4bb28b5c233f287e96f94b73ffa3a296ff163b94
Atto Allas <atto.allas@gmail.com>
2020-03-23 18:08:37 +0000

    Added Cryptopp (Joe has a small one)

--- library.cpp
+++ library.cpp
@@ -1,7 +1,33 @@
 #include "library.h"
 
 #include <iostream>
+#include <filters.h>
+#include <sha.h>
+#include <hex.h>
 
-void hello() {
+int main() {
+    using namespace CryptoPP;
+
+    byte password[] ="password";
+    size_t plen = strlen((const char*)password);
+
+    byte salt[] = "salt";
+    size_t slen = strlen((const char*)salt);
+
+    byte derived[SHA256::DIGESTSIZE];
+
+    PKCS5_PBKDF2_HMAC<SHA256> pbkdf;
+    byte unused = 0;
+
+    pbkdf.DeriveKey(derived, sizeof(derived), unused, password, plen, salt, slen, 1024, 0.0f);
+
+    std::string result;
+    HexEncoder encoder(new StringSink(result));
+
+    encoder.Put(derived, sizeof(derived));
+    encoder.MessageEnd();
+
+    std::cout << "Derived: " << result << std::endl;
+
     std::cout << "Hello, World!" << std::endl;
 }
--- library.h
+++ library.h
@@ -1,6 +1,8 @@
 #ifndef BEACON_CORE_LIBRARY_H
 #define BEACON_CORE_LIBRARY_H
 
+#include <pwdbased.h>
+
 void hello();
 
 #endif //BEACON_CORE_LIBRARY_H
